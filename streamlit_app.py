e0ce19db115a1ac35bea42df58eb95d681727f0f
mrmrmr7 <mrmrmr7@icloud.com>
2023-08-09 16:23:50 +0200

    regular code update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ import pandas
 
 df = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+df = df.set_index('Fruit')
+
 st.multiselect("Select fruit:", list(df.index))
 
 st.dataframe(df)
